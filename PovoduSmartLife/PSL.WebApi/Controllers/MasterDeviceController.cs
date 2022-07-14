abbb93acda64ceea7f659f1a3f46a8f22f7a00d3
povodu.digital <povodu.digital@gmail.com>
2022-07-15 01:01:53 +0300

    device bilgileri servisi temphelper ile yazıldı

---
+++ PovoduSmartLife/PSL.WebApi/Controllers/MasterDeviceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PSL.WebApi.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/master")]
+    [ApiController]
+    public class MasterDeviceController : ControllerBase
+    {
+        public MasterDeviceController()
+        {
+            TempHelper.Initialize();
+        }
+
+        [HttpGet("device")]
+        public async Task<IActionResult> GetDeviceByMacAddressAsync(string macAddress)
+        {
+            using (HttpResponseMessage response = await TempHelper.ApiClient.GetAsync("/device?macAddress=" + macAddress))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    string generatedDevice = await response.Content.ReadAsStringAsync();
+                    return Ok(generatedDevice);
+                }
+                else
+                {
+                    return BadRequest("çalışmadı");
+                }
+            }
+        }
+    }
+}
